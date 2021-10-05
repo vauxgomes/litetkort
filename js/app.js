b6ff2564068bca464c26f96170c3b86362744c9e
vaux.gomes <vauxsandino@gmail.com>
2021-10-05 18:39:52 -0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,54 @@
+// import Draggable
+Draggable(document.querySelector(".flip-card"), console.log);
+
+let data = [
+    {
+        question: "Qual é uma das principais características dos Poríferos?",
+        answer: "Presença de poros",
+        alternative: "Presença de tecidos e órgãos",
+        score: 0,
+    },
+    {
+        question: "Qual país tem um formato de uma bota?",
+        answer: "Itália",
+        alternative: "Brasil",
+        score: 0,
+    },
+    {
+        question: "Qual é o peixe mais lento do mundo?",
+        answer: "Cavalo-Marinho",
+        alternative: "Baleia",
+        score: 0,
+    },
+    {
+        question: "Qual é o maior órgão do corpo humano?",
+        answer: "Pele",
+        alternative: "Pulmões",
+        score: 0,
+    },
+    {
+        question: "Qual é o maior planeta do Sistema Solar?",
+        answer: "Júpiter",
+        alternative: "Terra",
+        score: 0,
+    },
+    {
+        question: 'De quem é a famosa frase "Penso, logo existo"?',
+        answer: "Descartes",
+        alternative: "Sócrates",
+        score: 0,
+    },
+    {
+        question: "Qual é menor osso do corpo?",
+        answer: "Estribo",
+        alternative: "Martelo",
+        score: 0,
+    },
+    {
+        question:
+            "O modo verbal que expressa uma atitude duvidosa, incerta é o:",
+        answer: "Subjuntivo",
+        alternative: "Indicativo",
+        score: 0,
+    },
+];
---
+++ js/draggable.js
@@ -0,0 +1,61 @@
+function Draggable(element, callbackFn) {
+    let length = 80;
+    let startX = 0;
+    let status = null;
+
+    let target = document.querySelector(element.getAttribute("data-target"));
+    let targetRight = element.getAttribute("data-right");
+    let targetWrong = element.getAttribute("data-wrong");
+
+    // Making it draggable
+    element.draggable = true;
+
+    // Drag start
+    ["dragstart", "touchstart"].forEach((evt) => {
+        element.addEventListener(evt, (e) => {
+            console.log("start");
+            startX = e.x;
+
+            if (!startX) startX = e.touches[0].clientX;
+        });
+    });
+
+    // Drag
+    ["drag", "touchmove"].forEach((evt) => {
+        element.addEventListener(evt, (e) => {
+            console.log("drag");
+
+            let userX = e.x;
+            let halfScreen = window.innerWidth / 2;
+
+            if (!userX) userX = e.touches[0].clientX;
+            if (userX === 0) return;
+
+            if (userX > halfScreen && userX >= startX + length) {
+                // console.log("right: ", userX, window.innerWidth / 2);
+                target.classList.add(targetRight);
+                target.classList.remove(targetWrong);
+                status = true;
+            } else if (userX < halfScreen && userX <= startX - length) {
+                // console.log("left: ", userX, window.innerWidth / 2);
+                target.classList.add(targetWrong);
+                target.classList.remove(targetRight);
+                status = false;
+            } else {
+                target.classList.remove(targetRight);
+                target.classList.remove(targetWrong);
+                status = null;
+            }
+        });
+    });
+
+    // Drag end
+    ["dragend", "touchend"].forEach((evt) => {
+        target.classList.remove(targetRight);
+        target.classList.remove(targetWrong);
+
+        element.addEventListener(evt, (e) => {
+            if (callbackFn) callbackFn(status);
+        });
+    });
+}
